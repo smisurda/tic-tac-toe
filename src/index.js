f1cb36c97338db02b29a8889da4abf0ef80a927d
Samantha Misurda <smisurda@simpleviewinc.com>
2018-11-22 18:40:50 -0700

    Final product with added rewind functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+/**
+ * @author Samantha L. Misurda
+ * index.js
+ * A React implementation of Tic Tac Toe
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './TicTacToe.css';
 
+/**
+ * Represents a single square on the board
+ */
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -10,51 +19,26 @@ function Square(props) {
   );
 }
 
+/**
+ * Maintains the board view
+ */
 class Board extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    };
-  }
 
+  /**
+   * Creates a square on the board
+   */
   renderSquare(i) {
     return(
       <Square 
-        value = {this.state.squares[i]} 
-        onClick = {() => this.handleClick(i)}
+        value = {this.props.squares[i]} 
+        onClick = {() => this.props.onClick(i)}
       />
     );
   }
 
-  handleClick(i){
-    const squares = this.state.squares.slice();
-
-    // Check if square has been used already or if there is a winner
-    if(calculateWinner(squares) || squares[i]){ 
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
-  }
-
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if(winner){
-      status = 'Winner: ' + winner;
-    }
-    else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -75,16 +59,87 @@ class Board extends React.Component {
   }
 }
 
+/**
+ * Manages the state of the game
+ */
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+      stepNumber: 0
+    };
+  }
+
+  /** 
+   * Responds to a user's mouse click on a board square
+   */
+  handleClick(i){
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    // If the square is populated or someone has won, end the game
+    if (calculateWinner(squares) || squares[i]) { 
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext,
+      stepNumber: history.length
+    });
+  }
+
+  /**
+   * Allows a player to rewind to a previous turn
+   */
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    })
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{ /* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{ status }</div>
+          <ol>{ moves }</ol>
         </div>
       </div>
     );
